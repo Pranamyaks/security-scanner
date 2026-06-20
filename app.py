81ccd5c116f068f76141bcea7e9f10c9577cad51
Pranamya <pranamyaks9@gmail.com>
2026-06-20 12:11:16 +0530

    Day 2: Added UI with input form

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
-app=Flask(__name__)
-@app.route("/")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "My Security Scanner is Running"
-if __name__=="__main__":
+    if request.method == "POST":
+        url = request.form["url"]
+        return f"You entered: {url}"
+    
+    return render_template("index.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
